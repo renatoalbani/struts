6b4851bef9b4510a2f6ba018341036e404b481c3
Renato <renato.utfpr@gmail.com>
2019-06-24 15:21:57 -0600

    Replatforming a Struts Application

--- src/main/java/org/superbiz/struts/App.java
+++ src/main/java/org/superbiz/struts/App.java
@@ -1,9 +1,12 @@
 package org.superbiz.struts;
 
 import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class App {
@@ -12,11 +15,25 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-//    @Bean
-//    public SiteMeshFilter siteMeshFilter(){
-//        SiteMeshFilter siteMeshFilter = new SiteMeshFilter();
-//        siteMeshFilter.init();
-//        return siteMeshFilter;
-//    }
+    @Bean
+    public FilterRegistrationBean siteMeshFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new SiteMeshFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean strutsPrepareAndExecuteFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new StrutsPrepareAndExecuteFilter());
+        filterRegistrationBean.addUrlPatterns("/");
+        filterRegistrationBean.addUrlPatterns("/addUserForm.action");
+        filterRegistrationBean.addUrlPatterns("/addUser.action");
+        filterRegistrationBean.addUrlPatterns("/findUserForm.action");
+        filterRegistrationBean.addUrlPatterns("/findUser.action");
+        filterRegistrationBean.addUrlPatterns("/listAllUsers.action");
+        return filterRegistrationBean;
+    }
 
 }
